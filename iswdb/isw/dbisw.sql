eb6e00cf325ad7698d9c5f8112db968b74d018c6
ingmamm <mamm155@hotmail.com>
2014-01-09 18:17:21 -0300

    scrip sql actualizado
    para que trabaje sobre el shema gurpo03

--- iswdb/isw/dbisw.sql
+++ iswdb/isw/dbisw.sql
@@ -13,57 +13,57 @@ DROP INDEX IF EXISTS provincias_nombre_region_fk_key;
 
 /* Drop Tables */
 
-DROP TABLE IF EXISTS public.cursos;
-DROP TABLE IF EXISTS public.alumno;
-DROP TABLE IF EXISTS public.cargos_academicos;
-DROP TABLE IF EXISTS public.cargos;
-DROP TABLE IF EXISTS public.competencias_academicas;
-DROP TABLE IF EXISTS public.competencias;
-DROP TABLE IF EXISTS public.formacion;
+DROP TABLE IF EXISTS grupo03.cursos;
+DROP TABLE IF EXISTS grupo03.alumno;
+DROP TABLE IF EXISTS grupo03.cargos_academicos;
+DROP TABLE IF EXISTS grupo03.cargos;
+DROP TABLE IF EXISTS grupo03.competencias_academicas;
+DROP TABLE IF EXISTS grupo03.competencias;
+DROP TABLE IF EXISTS grupo03.formacion;
 DROP TABLE IF EXISTS Participacion;
 DROP TABLE IF EXISTS TrabajoActual;
 DROP TABLE IF EXISTS LineasDeDesarrolloDocente;
-DROP TABLE IF EXISTS public.docentes;
-DROP TABLE IF EXISTS public.comunas;
-DROP TABLE IF EXISTS public.escuelas;
-DROP TABLE IF EXISTS public.departamentos;
-DROP TABLE IF EXISTS public.facultades;
-DROP TABLE IF EXISTS public.grados_academicos;
-DROP TABLE IF EXISTS public.instituciones;
-DROP TABLE IF EXISTS public.paises;
-DROP TABLE IF EXISTS public.provincias;
-DROP TABLE IF EXISTS public.regiones;
-DROP TABLE IF EXISTS public.tipos_instituciones;
+DROP TABLE IF EXISTS grupo03.docentes;
+DROP TABLE IF EXISTS grupo03.comunas;
+DROP TABLE IF EXISTS grupo03.escuelas;
+DROP TABLE IF EXISTS grupo03.departamentos;
+DROP TABLE IF EXISTS grupo03.facultades;
+DROP TABLE IF EXISTS grupo03.grados_academicos;
+DROP TABLE IF EXISTS grupo03.instituciones;
+DROP TABLE IF EXISTS grupo03.paises;
+DROP TABLE IF EXISTS grupo03.provincias;
+DROP TABLE IF EXISTS grupo03.regiones;
+DROP TABLE IF EXISTS grupo03.tipos_instituciones;
 
 
 
 /* Drop Sequences */
 
-DROP SEQUENCE IF EXISTS public.alumno_alumno_id_seq;
-DROP SEQUENCE IF EXISTS public.cargos_academicos_pk_seq;
-DROP SEQUENCE IF EXISTS public.cargos_pk_seq;
-DROP SEQUENCE IF EXISTS public.competencias_academicas_pk_seq;
-DROP SEQUENCE IF EXISTS public.competencias_pk_seq;
-DROP SEQUENCE IF EXISTS public.comunas_pk_seq;
-DROP SEQUENCE IF EXISTS public.cursos_curso_id_seq;
-DROP SEQUENCE IF EXISTS public.departamentos_pk_seq;
-DROP SEQUENCE IF EXISTS public.docentes_pk_seq;
-DROP SEQUENCE IF EXISTS public.escuelas_pk_seq;
-DROP SEQUENCE IF EXISTS public.facultades_pk_seq;
-DROP SEQUENCE IF EXISTS public.formacion_pk_seq;
-DROP SEQUENCE IF EXISTS public.grados_academicos_pk_seq;
-DROP SEQUENCE IF EXISTS public.instituciones_pk_seq;
-DROP SEQUENCE IF EXISTS public.paises_pk_seq;
-DROP SEQUENCE IF EXISTS public.provincias_pk_seq;
-DROP SEQUENCE IF EXISTS public.regiones_pk_seq;
-DROP SEQUENCE IF EXISTS public.tipos_instituciones_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.alumno_alumno_id_seq;
+DROP SEQUENCE IF EXISTS grupo03.cargos_academicos_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.cargos_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.competencias_academicas_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.competencias_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.comunas_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.cursos_curso_id_seq;
+DROP SEQUENCE IF EXISTS grupo03.departamentos_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.docentes_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.escuelas_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.facultades_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.formacion_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.grados_academicos_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.instituciones_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.paises_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.provincias_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.regiones_pk_seq;
+DROP SEQUENCE IF EXISTS grupo03.tipos_instituciones_pk_seq;
 
 
 
 
 /* Create Tables */
 
-CREATE TABLE public.alumno
+CREATE TABLE grupo03.alumno
 (
 	alumno_id serial NOT NULL,
 	nombre varchar(100) NOT NULL,
@@ -73,7 +73,7 @@ CREATE TABLE public.alumno
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.cargos
+CREATE TABLE grupo03.cargos
 (
 	pk serial NOT NULL,
 	nombre varchar(255) NOT NULL UNIQUE,
@@ -82,7 +82,7 @@ CREATE TABLE public.cargos
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.cargos_academicos
+CREATE TABLE grupo03.cargos_academicos
 (
 	pk bigserial NOT NULL,
 	cargo_fk int NOT NULL,
@@ -93,7 +93,7 @@ CREATE TABLE public.cargos_academicos
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.competencias
+CREATE TABLE grupo03.competencias
 (
 	pk serial NOT NULL,
 	nombre varchar(255) NOT NULL UNIQUE,
@@ -102,7 +102,7 @@ CREATE TABLE public.competencias
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.competencias_academicas
+CREATE TABLE grupo03.competencias_academicas
 (
 	pk bigserial NOT NULL,
 	competencia_fk int NOT NULL,
@@ -111,7 +111,7 @@ CREATE TABLE public.competencias_academicas
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.comunas
+CREATE TABLE grupo03.comunas
 (
 	pk serial NOT NULL,
 	nombre varchar(255) NOT NULL,
@@ -120,7 +120,7 @@ CREATE TABLE public.comunas
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.cursos
+CREATE TABLE grupo03.cursos
 (
 	curso_id serial NOT NULL,
 	curso varchar(100) NOT NULL,
@@ -129,7 +129,7 @@ CREATE TABLE public.cursos
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.departamentos
+CREATE TABLE grupo03.departamentos
 (
 	pk serial NOT NULL,
 	facultad_fk int NOT NULL,
@@ -139,7 +139,7 @@ CREATE TABLE public.departamentos
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.docentes
+CREATE TABLE grupo03.docentes
 (
 	pk bigserial NOT NULL,
 	nombres varchar(255) NOT NULL,
@@ -163,7 +163,7 @@ CREATE TABLE public.docentes
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.escuelas
+CREATE TABLE grupo03.escuelas
 (
 	pk serial NOT NULL,
 	departamento_fk int NOT NULL,
@@ -173,7 +173,7 @@ CREATE TABLE public.escuelas
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.facultades
+CREATE TABLE grupo03.facultades
 (
 	pk serial NOT NULL,
 	facultad varchar(255) NOT NULL UNIQUE,
@@ -182,7 +182,7 @@ CREATE TABLE public.facultades
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.formacion
+CREATE TABLE grupo03.formacion
 (
 	pk serial NOT NULL,
 	institucion_fk int NOT NULL,
@@ -195,7 +195,7 @@ CREATE TABLE public.formacion
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.grados_academicos
+CREATE TABLE grupo03.grados_academicos
 (
 	pk serial NOT NULL,
 	grado varchar(255) NOT NULL UNIQUE,
@@ -205,7 +205,7 @@ CREATE TABLE public.grados_academicos
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.instituciones
+CREATE TABLE grupo03.instituciones
 (
 	pk serial NOT NULL,
 	tipo_fk int NOT NULL,
@@ -215,7 +215,7 @@ CREATE TABLE public.instituciones
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.paises
+CREATE TABLE grupo03.paises
 (
 	pk serial NOT NULL,
 	cod_num int NOT NULL,
@@ -226,7 +226,7 @@ CREATE TABLE public.paises
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.provincias
+CREATE TABLE grupo03.provincias
 (
 	pk serial NOT NULL,
 	nombre varchar(255) NOT NULL,
@@ -235,7 +235,7 @@ CREATE TABLE public.provincias
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.regiones
+CREATE TABLE grupo03.regiones
 (
 	pk serial NOT NULL,
 	nombre varchar(255) NOT NULL UNIQUE,
@@ -246,7 +246,7 @@ CREATE TABLE public.regiones
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.tipos_instituciones
+CREATE TABLE grupo03.tipos_instituciones
 (
 	pk serial NOT NULL,
 	tipo varchar(255) NOT NULL UNIQUE,
@@ -292,73 +292,73 @@ CREATE TABLE LineasDeDesarrolloDocente
 
 /* Create Foreign Keys */
 
-ALTER TABLE public.cursos
+ALTER TABLE grupo03.cursos
 	ADD CONSTRAINT cursos_alumno_id_fkey FOREIGN KEY (alumno_id)
-	REFERENCES public.alumno (alumno_id)
+	REFERENCES grupo03.alumno (alumno_id)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.cargos_academicos
+ALTER TABLE grupo03.cargos_academicos
 	ADD CONSTRAINT cargos_academicos_cargo_fk_fkey FOREIGN KEY (cargo_fk)
-	REFERENCES public.cargos (pk)
+	REFERENCES grupo03.cargos (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.competencias_academicas
+ALTER TABLE grupo03.competencias_academicas
 	ADD CONSTRAINT competencias_academicas_competencia_fk_fkey FOREIGN KEY (competencia_fk)
-	REFERENCES public.competencias (pk)
+	REFERENCES grupo03.competencias (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.docentes
+ALTER TABLE grupo03.docentes
 	ADD CONSTRAINT docentes_comuna_fk_fkey FOREIGN KEY (comuna_fk)
-	REFERENCES public.comunas (pk)
+	REFERENCES grupo03.comunas (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.docentes
+ALTER TABLE grupo03.docentes
 	ADD CONSTRAINT docentes_departamento_fk_fkey FOREIGN KEY (departamento_fk)
-	REFERENCES public.departamentos (pk)
+	REFERENCES grupo03.departamentos (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.escuelas
+ALTER TABLE grupo03.escuelas
 	ADD CONSTRAINT escuelas_departamento_fk_fkey FOREIGN KEY (departamento_fk)
-	REFERENCES public.departamentos (pk)
+	REFERENCES grupo03.departamentos (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.cargos_academicos
+ALTER TABLE grupo03.cargos_academicos
 	ADD CONSTRAINT cargos_academicos_docente_fk_fkey FOREIGN KEY (docente_fk)
-	REFERENCES public.docentes (pk)
+	REFERENCES grupo03.docentes (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.competencias_academicas
+ALTER TABLE grupo03.competencias_academicas
 	ADD CONSTRAINT competencias_academicas_docente_fk_fkey FOREIGN KEY (docente_fk)
-	REFERENCES public.docentes (pk)
+	REFERENCES grupo03.docentes (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.formacion
+ALTER TABLE grupo03.formacion
 	ADD CONSTRAINT formacion_docente_fk_fkey FOREIGN KEY (docente_fk)
-	REFERENCES public.docentes (pk)
+	REFERENCES grupo03.docentes (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
@@ -366,7 +366,7 @@ ALTER TABLE public.formacion
 
 ALTER TABLE Participacion
 	ADD FOREIGN KEY (pk)
-	REFERENCES public.docentes (pk)
+	REFERENCES grupo03.docentes (pk)
 	ON UPDATE RESTRICT
 	ON DELETE RESTRICT
 ;
@@ -374,7 +374,7 @@ ALTER TABLE Participacion
 
 ALTER TABLE TrabajoActual
 	ADD FOREIGN KEY (pk)
-	REFERENCES public.docentes (pk)
+	REFERENCES grupo03.docentes (pk)
 	ON UPDATE RESTRICT
 	ON DELETE RESTRICT
 ;
@@ -382,63 +382,63 @@ ALTER TABLE TrabajoActual
 
 ALTER TABLE LineasDeDesarrolloDocente
 	ADD FOREIGN KEY (pk)
-	REFERENCES public.docentes (pk)
+	REFERENCES grupo03.docentes (pk)
 	ON UPDATE RESTRICT
 	ON DELETE RESTRICT
 ;
 
 
-ALTER TABLE public.departamentos
+ALTER TABLE grupo03.departamentos
 	ADD CONSTRAINT departamentos_facultad_fk_fkey FOREIGN KEY (facultad_fk)
-	REFERENCES public.facultades (pk)
+	REFERENCES grupo03.facultades (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.formacion
+ALTER TABLE grupo03.formacion
 	ADD CONSTRAINT formacion_grado_fk_fkey FOREIGN KEY (grado_fk)
-	REFERENCES public.grados_academicos (pk)
+	REFERENCES grupo03.grados_academicos (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.formacion
+ALTER TABLE grupo03.formacion
 	ADD CONSTRAINT formacion_institucion_fk_fkey FOREIGN KEY (institucion_fk)
-	REFERENCES public.instituciones (pk)
+	REFERENCES grupo03.instituciones (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.instituciones
+ALTER TABLE grupo03.instituciones
 	ADD CONSTRAINT instituciones_pais_fk_fkey FOREIGN KEY (pais_fk)
-	REFERENCES public.paises (pk)
+	REFERENCES grupo03.paises (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.comunas
+ALTER TABLE grupo03.comunas
 	ADD CONSTRAINT comunas_provincia_fk_fkey FOREIGN KEY (provincia_fk)
-	REFERENCES public.provincias (pk)
+	REFERENCES grupo03.provincias (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.provincias
+ALTER TABLE grupo03.provincias
 	ADD CONSTRAINT provincias_region_fk_fkey FOREIGN KEY (region_fk)
-	REFERENCES public.regiones (pk)
+	REFERENCES grupo03.regiones (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE public.instituciones
+ALTER TABLE grupo03.instituciones
 	ADD CONSTRAINT instituciones_tipo_fk_fkey FOREIGN KEY (tipo_fk)
-	REFERENCES public.tipos_instituciones (pk)
+	REFERENCES grupo03.tipos_instituciones (pk)
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 ;
@@ -447,13 +447,13 @@ ALTER TABLE public.instituciones
 
 /* Create Indexes */
 
-CREATE UNIQUE INDEX cargos_academicos_cargo_fk_docente_fk_inicio_termino_key ON public.cargos_academicos USING BTREE (cargo_fk, docente_fk, inicio, termino);
-CREATE UNIQUE INDEX competencias_academicas_competencia_fk_docente_fk_key ON public.competencias_academicas USING BTREE (competencia_fk, docente_fk);
-CREATE UNIQUE INDEX comunas_nombre_provincia_fk_key ON public.comunas USING BTREE (nombre, provincia_fk);
-CREATE UNIQUE INDEX docentes_nombres_apellidos_rut_email_key ON public.docentes USING BTREE (nombres, apellidos, rut, email);
-CREATE UNIQUE INDEX instituciones_tipo_fk_nombre_pais_fk_key ON public.instituciones USING BTREE (tipo_fk, nombre, pais_fk);
-CREATE UNIQUE INDEX paises_cod_num_alfa_dos_alfa_tres_key ON public.paises USING BTREE (cod_num, alfa_dos, alfa_tres);
-CREATE UNIQUE INDEX provincias_nombre_region_fk_key ON public.provincias USING BTREE (nombre, region_fk);
+CREATE UNIQUE INDEX cargos_academicos_cargo_fk_docente_fk_inicio_termino_key ON grupo03.cargos_academicos USING BTREE (cargo_fk, docente_fk, inicio, termino);
+CREATE UNIQUE INDEX competencias_academicas_competencia_fk_docente_fk_key ON grupo03.competencias_academicas USING BTREE (competencia_fk, docente_fk);
+CREATE UNIQUE INDEX comunas_nombre_provincia_fk_key ON grupo03.comunas USING BTREE (nombre, provincia_fk);
+CREATE UNIQUE INDEX docentes_nombres_apellidos_rut_email_key ON grupo03.docentes USING BTREE (nombres, apellidos, rut, email);
+CREATE UNIQUE INDEX instituciones_tipo_fk_nombre_pais_fk_key ON grupo03.instituciones USING BTREE (tipo_fk, nombre, pais_fk);
+CREATE UNIQUE INDEX paises_cod_num_alfa_dos_alfa_tres_key ON grupo03.paises USING BTREE (cod_num, alfa_dos, alfa_tres);
+CREATE UNIQUE INDEX provincias_nombre_region_fk_key ON grupo03.provincias USING BTREE (nombre, region_fk);
 
 
 
